81d03d21ba564996b4666fa2cbb50ffda16f3761
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-11-22 00:40:34 +0300

    Add TryingFramework.swift

---
+++ EdgeKit/TryingFramework.swift
@@ -0,0 +1,14 @@
+//
+//  TryingFramework.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 22.11.2024.
+//
+
+import Foundation
+
+struct TryingFramework {
+    
+    static var text = "Hello, World!"
+    
+}
