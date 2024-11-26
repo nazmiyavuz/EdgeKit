8fc1d5e0ba0cde230cf3ec53719c84f5183ccab7
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-11-26 19:30:37 +0300

    Add Test.swift

---
+++ Sources/EdgeKit/EdgeKit/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 26.11.2024.
+//
+
+import Foundation
+
+public struct Test {
+    public init() {}
+    
+    public func test() {
+        print("Hello, World!")
+    }
+}
