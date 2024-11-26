6e7fbbfa60016adc91388adaf4d7ae0c51305c2a
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-11-26 16:26:26 +0300

    Add EdgeKit

---
+++ EdgeKit/EdgeKit/EdgeKit.h
@@ -0,0 +1,18 @@
+//
+//  EdgeKit.h
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 26.11.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EdgeKit.
+FOUNDATION_EXPORT double EdgeKitVersionNumber;
+
+//! Project version string for EdgeKit.
+FOUNDATION_EXPORT const unsigned char EdgeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EdgeKit/PublicHeader.h>
+
+
