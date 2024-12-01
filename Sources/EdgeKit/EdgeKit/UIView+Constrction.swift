d83a0434872957d4b94fd881613b1b56e1821a9b
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-01 16:19:19 +0300

    Add UIView+Constrction.swift

---
+++ Sources/EdgeKit/EdgeKit/UIView+Constrction.swift
@@ -0,0 +1,44 @@
+//
+//  UIView+Constrction.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 1.12.2024.
+//
+
+import UIKit
+
+extension UIView: Construction {
+    
+    @discardableResult
+    public func left(
+        toItem view: HorizontalEdge,
+        padding: CGFloat = 0,
+        relatedBy: AnchorRelation = .equal,
+        priority: UILayoutPriority = .required,
+        isActive: Bool = true
+    ) -> NSLayoutConstraint {
+        arrangeForAutoLayout()
+        
+        var constraint: NSLayoutConstraint {
+            switch relatedBy {
+            case .equal:
+                return leftAnchor.constraint(equalTo: view.anchor, constant: padding)
+                
+            case .greaterThanOrEqual:
+                return leftAnchor.constraint(greaterThanOrEqualTo: view.anchor, constant: padding)
+                
+            case .lessThanOrEqual:
+                return leftAnchor.constraint(lessThanOrEqualTo: view.anchor, constant: padding)
+                
+            @unknown default:
+                return leftAnchor.constraint(equalTo: view.anchor, constant: padding)
+            }
+        }
+        
+        return constraint.setPriority(to: priority).activate(isActive)
+    }
+    
+}
+    
+    
+
