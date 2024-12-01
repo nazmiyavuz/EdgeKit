757862b6ef6356ccd38677bab205d2bcd18142d0
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-01 16:18:46 +0300

    Add Construction.swift

---
+++ Sources/EdgeKit/EdgeKit/Construction.swift
@@ -0,0 +1,21 @@
+//
+//  Construction.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 1.12.2024.
+//
+
+
+import UIKit
+
+public typealias AnchorRelation = NSLayoutConstraint.Relation
+
+public protocol Construction {
+    func left(
+        toItem view: HorizontalEdge,
+        padding: CGFloat,
+        relatedBy: AnchorRelation,
+        priority: UILayoutPriority,
+        isActive: Bool
+    ) -> NSLayoutConstraint
+}
