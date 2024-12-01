21906d4cb064586b870e11f455519a8c25fa36d6
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-01 16:06:51 +0300

    Change the name of VerticalAnchor to VerticalEdge

---
+++ Sources/EdgeKit/EdgeKit/VerticalEdge.swift
@@ -0,0 +1,23 @@
+//
+//  VerticalEdge.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 1.12.2024.
+//
+
+
+import UIKit
+
+public enum VerticalEdge {
+    case topEdge(of: UIView)
+    case bottomEdge(of: UIView)
+    case centerY(of: UIView)
+    
+    public var anchor: NSLayoutYAxisAnchor {
+        switch self {
+        case .topEdge(of: let view):     return view.topAnchor
+        case .bottomEdge(of: let view):  return view.bottomAnchor
+        case .centerY(of: let view):     return view.centerYAnchor
+        }
+    }
+}
