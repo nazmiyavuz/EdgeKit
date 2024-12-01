e1facf4a1714cd80b40b3600e177b2ba0eeaf314
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-01 22:53:25 +0300

    Add the documentation of left()

--- Sources/EdgeKit/EdgeKit/Protocols/Construction.swift
+++ Sources/EdgeKit/EdgeKit/Protocols/Construction.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
+/// A new type to handle a clean code logic
 public typealias AnchorRelation = NSLayoutConstraint.Relation
 
 @MainActor
 public protocol Construction {
+    
+    /// The left alignment of the related view according to the another one
+    /// - Parameters:
+    ///   - view: The first attribute in a constraint.
+    ///   - padding: The constant offset for the constraint.
+    ///   - relatedBy: The relation between the first attribute
+    ///   and the modified second attribute in a constraint.
+    ///   - priority: The layout priority is used to indicate to the
+    ///   constraint-based layout system which constraints are more important,
+    ///   allowing the system to make appropriate tradeoffs when satisfying
+    ///   the constraints of the system as a whole.
+    ///   - isActive: A boolean property to activate or deactivate the constraint
+    /// - Returns: The relationship between two user interface objects that
+    ///   must be satisfied by the constraint-based layout system.
     func left(
         toItem view: HorizontalEdge,
         padding: CGFloat,
