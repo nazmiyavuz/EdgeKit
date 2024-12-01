8ae4194a6b39064399d80f3c93a553b68598ab1c
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-01 16:06:24 +0300

    Change the name of HorizontalAnchor to HorizontalEdge

---
+++ Sources/EdgeKit/EdgeKit/HorizontalEdge.swift
@@ -0,0 +1,27 @@
+//
+//  HorizontalEdge.swift
+//  EdgeKit
+//
+//  Created by Nazmi Yavuz on 1.12.2024.
+//
+
+
+import UIKit
+
+public enum HorizontalEdge {
+    
+    case leftEdge(of: UIView)
+    case leadingEdge(of: UIView)
+    case trailingEdge(of: UIView)
+    case centerX(of: UIView)
+    
+    public var anchor: NSLayoutXAxisAnchor {
+        switch self {
+        case .leftEdge(of: let view):     return view.leftAnchor
+        case .leadingEdge(of: let view):  return view.leadingAnchor
+        case .trailingEdge(of: let view): return view.trailingAnchor
+        case .centerX(of: let view):      return view.centerXAnchor
+        }
+    }
+    
+}
