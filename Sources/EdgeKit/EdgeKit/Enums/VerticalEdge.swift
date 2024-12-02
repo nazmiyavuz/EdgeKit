bb4c96c24b3cf6b6258aa043c08a715d75ea2858
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-02 10:17:51 +0300

    Create VerticalEdge

--- Sources/EdgeKit/EdgeKit/Enums/VerticalEdge.swift
+++ Sources/EdgeKit/EdgeKit/Enums/VerticalEdge.swift
@@ -10,15 +10,57 @@ import UIKit
 
 @MainActor
 public enum VerticalEdge {
-    case topEdge(of: UIView)
-    case bottomEdge(of: UIView)
-    case centerY(of: UIView)
     
-    public var anchor: NSLayoutYAxisAnchor {
+    case parentView(inSafeArea: Bool)
+    case topEdge(of: UIView?)
+    case bottomEdge(of: UIView?)
+    case centerY(of: UIView?)
+    
+    public var anchor: NSLayoutYAxisAnchor? {
         switch self {
-        case .topEdge(of: let view):     return view.topAnchor
-        case .bottomEdge(of: let view):  return view.bottomAnchor
-        case .centerY(of: let view):     return view.centerYAnchor
+        case .topEdge(of: let view):     return view?.topAnchor
+        case .bottomEdge(of: let view):  return view?.bottomAnchor
+        case .centerY(of: let view):     return view?.centerYAnchor
+        case .parentView:                return nil
+        }
+    }
+    
+    func setParentAnchor(of view: UIView?, edge: Self) -> NSLayoutYAxisAnchor? {
+        switch self {
+        case .parentView(inSafeArea: let inSafe):
+            return getParentViewAnchor(for: view, inSafeArea: inSafe, edge: edge)
+            
+        case .topEdge, .bottomEdge, .centerY:
+            return nil
+        }
+    }
+    
+    // MARK: - Private Functions
+    private func getParentView(of view: UIView?) -> UIView? {
+        guard let superview = view else {
+            assertionFailure("There is not any superview of this view.")
+            return nil
+        }
+        return superview
+    }
+    
+    private func getParentViewAnchor(for view: UIView?, inSafeArea: Bool, edge: Self) -> NSLayoutYAxisAnchor? {
+        guard let parentView = getParentView(of: view) else {
+            return nil
+        }
+        
+        switch edge {
+        case .parentView:
+            return nil
+            
+        case .topEdge:
+            return inSafeArea ? parentView.safeAreaLayoutGuide.topAnchor : parentView.topAnchor
+            
+        case .bottomEdge:
+            return inSafeArea ? parentView.safeAreaLayoutGuide.bottomAnchor : parentView.bottomAnchor
+            
+        case .centerY:
+            return inSafeArea ? parentView.safeAreaLayoutGuide.centerYAnchor : parentView.centerYAnchor
         }
     }
 }
