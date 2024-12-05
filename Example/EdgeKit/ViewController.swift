d287c6c6f8610cd0b83be395d9f217fe06e16505
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-05 23:38:31 +0800

    Set the deployment targets

--- Example/EdgeKit/ViewController.swift
+++ Example/EdgeKit/ViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import EdgeKit
 
 class ViewController: UIViewController {
 
+    private let firstView: UIView = {
+        $0.backgroundColor = .systemPurple
+        $0.layer.cornerRadius = 5
+        return $0
+    }(UIView())
+    
+    private let secondView: UIView = {
+        $0.backgroundColor = .systemOrange
+        $0.layer.cornerRadius = 5
+        return $0
+    }(UIView())
+    
+    private let helloWorld: UILabel = {
+        $0.text = "Hello World"
+        $0.font = UIFont.boldSystemFont(ofSize: 12)
+        return $0
+    }(UILabel())
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        
+        view.addSubview(firstView)
+        firstView.top(to: .parentView(inSafeArea: true), padding: 16)
+        firstView.left(to: .parentView(inSafeArea: true), padding: 16)
+        firstView.setSize(width: 200, height: 200)
+        
+        view.addSubview(secondView)
+        secondView.anchor(leftAnchor: .leftEdge(of: firstView), topAnchor: .bottomEdge(of: firstView),
+                          rightAnchor: .rightEdge(of: firstView), topPadding: 20)
+        secondView.setHeight(100)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
