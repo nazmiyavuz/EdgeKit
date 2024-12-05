4af84fbbe63c9e4d6f4de5fa32f91083c1c8f293
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-05 23:39:25 +0800

    Change iOS platform version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EdgeKit",
     platforms: [
-      .macOS(.v12), .iOS(.v13)
+      .macOS(.v12), .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
