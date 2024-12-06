57ef9ae4f69fc6907d2bccbd3d0e7d411decbf03
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-06 13:33:38 +0800

    docs: Change the docs places

--- EdgeKit/Classes/Extensions/UIView+Anchorable.swift
+++ EdgeKit/Classes/Extensions/UIView+Anchorable.swift
@@ -9,16 +9,12 @@ import UIKit
 
 extension UIView: Anchorable {
 
-    /// A Function to set the view’s autoresizing mask to false
-    /// in order to determine whether the view’s autoresizing mask is translated
-    /// into Auto Layout constraints.
     public func arrangeForAutoLayout() {
         if translatesAutoresizingMaskIntoConstraints {
             translatesAutoresizingMaskIntoConstraints = false
         }
     }
     
-    /// A function to remove all constraints of subviews of the related UIView
     public func removeAllConstraints() {
         for view in self.subviews {
             view.removeConstraints(view.constraints)
--- EdgeKit/Classes/Protocols/Anchorable.swift
+++ EdgeKit/Classes/Protocols/Anchorable.swift
@@ -9,6 +9,12 @@ import UIKit
 
 @MainActor
 public protocol Anchorable {
+    
+    /// A Function to set the view’s autoresizing mask to false
+    /// in order to determine whether the view’s autoresizing mask is translated
+    /// into Auto Layout constraints.
     func arrangeForAutoLayout()
+    
+    /// A function to remove all constraints of subviews of the related UIView.
     func removeAllConstraints()
 }
